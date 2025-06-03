52a412bfa12f488a1191372b46e0cd2228772523
bardyaxy <bardiabanihashemi@gmail.com>
2025-06-03 15:35:19 -0700

    Add watch mode and document build steps

---
+++ scripts/watch.js
@@ -0,0 +1,33 @@
+const chokidar = require('chokidar');
+const { spawn } = require('child_process');
+
+const watchPaths = [
+  'scripts/**/*.js',
+  'styles/**/*.css',
+  'includes/**/*.html',
+  '*.html',
+  'webpack.config.js',
+];
+
+function runBuild() {
+  const proc = spawn('node', ['scripts/build.js'], { stdio: 'inherit' });
+  proc.on('close', (code) => {
+    if (code !== 0) {
+      console.error(`Build exited with code ${code}`);
+    }
+  });
+}
+
+runBuild();
+
+const watcher = chokidar.watch(watchPaths, {
+  ignored: ['dist/**', 'node_modules/**'],
+  ignoreInitial: true,
+});
+
+watcher.on('all', (event, path) => {
+  console.log(`${event} ${path} - rebuilding...`);
+  runBuild();
+});
+
+console.log('Watching for changes. Press Ctrl+C to exit.');
